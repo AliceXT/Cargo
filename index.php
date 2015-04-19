051120eb8e20ab6cd2705064f92db69190042e83
AliceXT <634771820@qq.com>
2015-04-19 22:29:38 +0800

    add file

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>Cargo|汽车网上交易平台</title>
+	<meta nsmr="keywords" content="买二手车，汽车交易，买车，卖车">
+	<meta name="description" content="Cargo是网上汽车交易平台，提供卖方信息供买主更容易的找到需要的购车信息">
+	<meta name="renderer" content="webkit">
+	<link href="css/main.css" rel="stylesheet" type="text/css">
+	<link href="css/index.css" rel="stylesheet" type="text/css">
+	<script type="text/javascript">
+		var msgCt = -1;
+	</script>
+	<link id="favicon" href="image/logo.png" rel="icon" >
+	<script src="js/mootools-core.js"></script>
+	<script src="js/mootools-more.js" type="text/javascript"></script>
+	<script src="js/comm.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		function a0UserClient(){
+			return {
+				'get':function(){
+					return null;
+				}
+			}
+		};
+	</script>
+	<script type="text/javascript" src="js/logger.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/bdsstyle.css">
+	<!--<script type="text/javascript" src="js/prototype-1.5.1.js"></script>-->
+	<!--<script type="text/javascript" src="js/json_index.js"></script>-->
+</head>
+<body>
+    <div>
+        <?php include("header.php");?>
+
+        <!--head 结束-->
+
+        <?php include("wapper.php"); ?>
+    </div>
+    <?php include("endjs.php"); ?>
+	
+
+</body>	
+</html>
+	
