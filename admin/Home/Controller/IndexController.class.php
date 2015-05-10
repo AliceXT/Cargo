0772f7c8a383d00591b254b2317970d5be6de0c9
Xueting Fang <634771820@qq.com>
2015-05-10 16:24:45 +0800

    前端后台数据传输解决

--- admin/Home/Controller/IndexController.class.php
+++ admin/Home/Controller/IndexController.class.php
@@ -1,7 +1,6 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-use Org\Post\HttpClient;
 class IndexController extends Controller {
     public function index(){
         header("Content-Type:text/html; charset=utf-8");
@@ -10,13 +9,19 @@ class IndexController extends Controller {
 
     public function makePost(){
         header("Content-Type:text/html; charset=utf-8");
-    	$url = "http://127.0.0.1:80/test/index.php";
-    	$params=array('name'=>$_POST['username'],'password'=>$_POST['password']);  	
-    	$pageContents = HttpClient::quickPost($url,$params);
-    	$result=explode(',',$pageContents);
-    	dump($pageContents);
-            
-    }
+        $url = "http://127.0.0.1/test/index.php";
+        $post_data = array("name"=>$_POST['username'],
+            "password"=>$_POST['password']);
 
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNRANSFER, 1);
+        //设置为POST
+        curl_setopt($ch, CURLOPT_POST, 1);
+        //把post变量加上
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+        $output = curl_exec($ch);
+        dump($output);
+    }
 }
 
